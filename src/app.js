c9f97add3bb7397c9b5fc4ee0759216a3a9605f2
Ivaaaan02 <ivanaldousvillafuerte2@gmail.com>
2023-06-02 19:26:09 +0800

    second commit

--- src/app.js
+++ src/app.js
@@ -1,3 +1,39 @@
-const { v4: uuidv4 } = require('uuid');
+const e = require('express');
+const express = require('express');
+const app = express();
 
-console.log(uuidv4());
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
+const PORT = 3000;
+
+const customers = [
+    {
+        "Name": "Ivan",
+        "Industry": "Student"
+    },
+    {
+        "Name": "ASG",
+        "Industry": "IT"
+    }
+];
+
+app.get('/', (req, res) => {
+    res.send('Welcome!');
+});
+
+app.get('/api/customers', (req, res) => {
+    res.send({"customers": customers});
+});
+
+app.post('/api/customers', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
+
+app.post('/', (req, res) => {
+    res.send('This is a post request!');
+});
+
+app.listen(PORT, () => {
+    console.log('App listening on port ' + PORT);
+})
